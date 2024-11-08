12801a8dac608e50306f6284a6af54194ce501bc
sh hack <rialcoin.tm@gmail.com>
2024-11-08 08:54:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import requests
 
+data = {
+  "otp_call": "false",
+  "username": "09*********"
+}
+
+response = requests.post('https://api.digikala.com/v1/user/authenticate/', json=data)
+
+print(response.status_code)
